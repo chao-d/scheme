3dc4c991e2b942767d7baf97e1e9704cf4628967
Chao Duan <chao0duan@gmail.com>
2021-09-10 22:48:47 -0400

    chapter 3: list with atoms done

--- little.scm
+++ little.scm
@@ -62,3 +62,23 @@
         )
     )
 )
+
+(define multirember
+    (lambda (a lat)
+        (cond
+            ((null? lat) '())
+            ((eq? a (car lat)) (multirember a (cdr lat)))
+            (else (cons (car lat) (multirember a (cdr lat))))
+        )    
+    )
+)
+
+(define multisubst
+    (lambda (new old lat)
+        (cond
+            ((null? lat) '())
+            ((eq? old (car lat)) (cons new (multisubst new old (cdr lat))))
+            (else (cons (car lat) (multisubst new old (cdr lat))))
+        )
+    )
+)
